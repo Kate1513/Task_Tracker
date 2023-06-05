30bdb6f524fd0c632127f8c46f1580d3409429f4
Kate1513 <kate_1513@hotmail.com>
2023-06-04 20:25:45 -0500

    Configuración de Firebase y Firestore, funciones de autenticación

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <>
+      <h2>Error 404</h2>
+      <h3>Page Not Found</h3>
+    </>
+  )
+}
+
+export { PageNotFound }
---
+++ src/services/auth.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { setDoc, doc } from 'firebase/firestore'
+import { auth, db } from './firebase'
+import { useNavigate } from 'react-router-dom'
+import { PropTypes } from 'prop-types'
+
+const AuthContext = React.createContext()
+
+function AuthProvider({ children }) {
+  const navigate = useNavigate()
+  const [loggedUser, setLoggedUser] = React.useState(null)
+
+  // Create user with email and password
+  const signUpUser = async (email, password, nickname) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      registerDocUser(userCredential.user.uid, nickname)
+      navigate('/')
+    } catch {
+      throw new Error()
+    }
+  }
+
+  // Login with email and password
+  const loginUser = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      setLoggedUser(userCredential)
+    } catch {
+      throw new Error('No existe el usuario')
+    }
+  }
+
+  // Create User Document in Firestore.
+  const registerDocUser = async (uid, nickname) => {
+    await setDoc(doc(db, 'Users', uid), {
+      name: nickname,
+    })
+  }
+
+  const authUser = { loggedUser, signUpUser, loginUser }
+
+  return <AuthContext.Provider value={authUser}>{children}</AuthContext.Provider>
+}
+
+function useAuth() {
+  const authContext = React.useContext(AuthContext)
+  return authContext
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.object,
+}
+
+export { AuthProvider, useAuth }
