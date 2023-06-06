6cc4d2198868cb2fe259a3e1b3754c248e0d7d64
Kate1513 <kate_1513@hotmail.com>
2023-06-06 08:44:18 -0500

    Cambio de DB de Firestore a RealTime

---
+++ src/services/realTimeDB.js
@@ -0,0 +1,66 @@
+import { ref, set, get, onValue, update, remove, child, push } from 'firebase/database'
+import { db } from './firebase'
+
+// Create User in the DB.
+const registerUser = async (uid, nickname) => {
+  await set(ref(db, 'users/' + uid), {
+    name: nickname,
+  })
+}
+
+// Get user data from the DB.
+const getUserData = async (uid) => {
+  const userSnap = get(child(db, `users/${uid}`))
+  if (userSnap.exists()) {
+     return userSnap.val()
+  } else {
+      throw new Error('No existe el usuario en la base de datos.')
+  }
+}
+
+// Get user toDos.
+const getUserToDos = async (uid) => {
+  const userToDos = onValue(ref(db, `user-todos/${uid}`))
+  if (userToDos.exists()) {
+    return userToDos.val()
+  } else {
+      throw new Error('No existe el usuario en la base de datos.')
+  }
+}
+
+// Add new toDo.
+const addToDo = async(uid, toDo)=>{
+  try{
+    const newToDoId = push(child(ref(db, 'user-todos/'))).key
+    const updatePost = {}
+    updatePost[`/user-todos/${uid}/${newToDoId}`] = toDo
+    return update(ref(db), updatePost)
+  }
+  catch{
+    throw new Error('Error al agregar la tarea.')
+  }  
+}
+
+// Edit toDo.
+const editToDo = async(uid, toDoId, toDo)=>{
+  try{
+    const updatePost = {}
+    updatePost[`/user-todos/${uid}/${toDoId}`] = toDo
+    return update(ref(db), updatePost)
+  }
+  catch{
+    throw new Error('Error al actualizar la tarea.')
+  }  
+}
+
+// Remove toDo.
+const removeToDo = async(uid, toDoIdToBeRemove)=>{
+  try{
+    remove(ref(db, `user-todos/${uid}/${toDoIdToBeRemove}`))
+  }
+  catch{
+    throw new Error('Error al eliminar la tarea.')
+  }  
+}
+
+export { registerUser, getUserData, getUserToDos, addToDo, editToDo, removeToDo }
